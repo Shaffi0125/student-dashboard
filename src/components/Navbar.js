64952afcfab64cad6ab9aef11684d552e2aae789
shaffijindal58876@gmail.com <shaffijindal58876@gmail.com>
2025-01-12 12:41:27 +0530

    my-Fifth-commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ import React from 'react'
 const Navbar = () => {
     return (
         <nav className='items-center flex justify-between p-5 border-b'>
-            <Image className=' invert' src={'/logo.svg'} alt="logo" width={220} height={220}/>
+            <Image src={'/logo2.png'} alt="logo" width={200} height={200} />
             <div className='items-center flex justify-between gap-2 border p-2 rounded-md'>
                 <div className=' rounded-full overflow-hidden border w-7 h-7 relative' >
-                <Image className=' absolute' src={'/profile-img.jpg'} alt="profile-img" fill />
+                    <Image className=' absolute' src={'/profile-img.jpg'} alt="profile-img" fill />
                 </div>
                 <p className=' text-black text-lg font-bold'>Shaffi</p>
             </div>
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -12,34 +12,34 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-  } from "@/components/ui/dialog"
+} from "@/components/ui/dialog"
 import { MoveRight } from 'lucide-react'
-  
+
 
 const Skill = () => {
 
     const [rank, setRank] = useState(1);
     const [percentile, setPercentile] = useState(1);
-    const [score, setScore] = useState(3);
+    const [score, setScore] = useState(1);
     const [rankInput, setRankInput] = useState(1);
     const [percentileInput, setPercentileInput] = useState(1);
     const [scoreInput, setScoreInput] = useState(1);
     const [showInput, setShowInput] = useState(false);
 
-    const handleSave = ()=>{
-        if(scoreInput > 15){
+    const handleSave = () => {
+        if (scoreInput > 15) {
             alert("Score should be less than or equal to 15");
             return;
-        } else if(scoreInput < 0){
+        } else if (scoreInput < 0) {
             alert("Minimum score is 0");
             return;
-        } else if(percentileInput < 0){
+        } else if (percentileInput < 0) {
             alert("Minimum percentile is 0");
             return;
-        } else if(percentileInput > 100){
+        } else if (percentileInput > 100) {
             alert("Percentile should be less than or equal to 100");
             return;
-        } else if(rankInput < 0){
+        } else if (rankInput < 0) {
             alert("Minimum Rank is 0");
             return;
         }
@@ -62,27 +62,27 @@ const Skill = () => {
                                 <p>Questions: 08 | Duration: 15 mins | Submitted on 5 June 2021</p>
                             </div>
                             <Dialog>
-                                <DialogTrigger onClick={()=>setShowInput(true)} className='font-bold bg-blue-700 p-2 rounded-lg text-white'>Update</DialogTrigger>
+                                <DialogTrigger onClick={() => setShowInput(true)} className='font-bold bg-blue-700 p-2 rounded-lg text-white'>Update</DialogTrigger>
                                 {showInput && <DialogContent>
                                     <DialogHeader>
                                         <DialogTitle className=" flex justify-between items-center mb-4">
-                                            Update Scores 
+                                            Update Scores
                                             <div className='relative h-8 w-8'><Image className=' absolute' src={'/html-logo.png'} alt="html-logo" fill /></div>
                                         </DialogTitle>
                                         <div className=' grid grid-cols-[20px_310px_120px] gap-3'>
                                             <div className='border flex justify-center rounded-full bg-blue-950 text-white'> 1 </div>
                                             <div> Update your <b>Rank</b> </div>
-                                            <input className='border border-blue-500 pl-2 rounded-sm' value={rankInput} type='number' onChange={(e)=>{setRankInput(e.target.value)}}></input>
+                                            <input className='border border-blue-500 pl-2 rounded-sm' value={rankInput} type='number' onChange={(e) => { setRankInput(e.target.value) }}></input>
                                             <div className='border flex justify-center rounded-full bg-blue-950 text-white'> 2 </div>
                                             <div> Update your <b>Percentile</b> </div>
-                                            <input className='border border-blue-500 pl-2 rounded-sm' type='number' value={percentileInput} onChange={(e)=>{setPercentileInput(e.target.value)}}></input>
+                                            <input className='border border-blue-500 pl-2 rounded-sm' type='number' value={percentileInput} onChange={(e) => { setPercentileInput(e.target.value) }}></input>
                                             <div className='border flex justify-center rounded-full bg-blue-950 text-white'> 3 </div>
                                             <div> Update your <b>current score (out of 15)</b> </div>
-                                            <input className='border border-blue-500 pl-2 rounded-sm' type='number'value={scoreInput} onChange={(e)=>{setScoreInput(e.target.value)}}></input>
+                                            <input className='border border-blue-500 pl-2 rounded-sm' type='number' value={scoreInput} onChange={(e) => { setScoreInput(e.target.value) }}></input>
                                         </div>
                                         <div className='flex justify-end gap-4 pt-4'>
-                                        <button onClick={()=>{setShowInput(false)}} className='border p-2 rounded-sm text-blue-950 text-xs font-bold border-black'>Cancel</button>
-                                        <button onClick={handleSave} className='border p-3 rounded-sm bg-blue-950 text-white flex gap-5'>Save <MoveRight/> </button>
+                                            <button onClick={() => { setShowInput(false) }} className='border p-2 rounded-sm text-blue-950 text-xs font-bold border-black'>Cancel</button>
+                                            <button onClick={handleSave} className='border p-3 rounded-sm bg-blue-950 text-white flex gap-5'>Save <MoveRight /> </button>
                                         </div>
                                     </DialogHeader>
                                 </DialogContent>}
@@ -90,11 +90,11 @@ const Skill = () => {
                         </div>
                         <Input score={score} percentile={percentile} rank={rank} />
                     </div>
-                    <Graph percentile={percentile}/>
+                    <Graph percentile={percentile} />
                 </div>
                 <div className=' flex flex-col gap-4 flex-grow'>
                     <Syllabus />
-                    <Question score={score}/>
+                    <Question score={score} />
                 </div>
             </div>
         </div>
